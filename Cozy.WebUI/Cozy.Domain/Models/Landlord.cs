2f36263a5141b985b030c6d46847e5596070dce3
Kyung Myung Ko <peanut61313@gmail.com>
2019-01-29 01:35:27 -0600

    class notes

---
+++ Cozy.WebUI/Cozy.Domain/Models/Landlord.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cozy.Domain.Models
+{
+    public class Landlord
+    {
+        public string Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+    }
+}
